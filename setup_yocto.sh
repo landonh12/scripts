dccece87be4f2776e07e23471af59815f98bb970
Landon <landon.haugh@nxp.com>
2023-11-27 10:45:20 -0600

    yocto script update

--- setup_yocto.sh
+++ setup_yocto.sh
@@ -44,6 +44,6 @@ cd imx-yocto-bsp
 /home/$USER/bin/repo sync
 
 echo "You may now set up a build folder. Please run the following command:"
-echo "DISTRO=<distro name> MACHINE=<machine name> source imx-setup-release.sh -b <build-dir>"
+echo "cd ~/imx-yocto-bsp; DISTRO=<distro name> MACHINE=<machine name> source imx-setup-release.sh -b <build-dir>"
 echo "Here is a list of DISTRO names: fsl-imx-wayland, fsl-imx-xwayland, fsl,imx-fb"
-echo "For MACHINE names, see the following link: "
+echo "For MACHINE names, please refer to the i.MX Yocto Project User's Guide for your BSP version."
