c1b607cf3c4be962344bffbbf964fc55b243e634
Landon <landon.haugh@nxp.com>
2024-02-02 10:12:23 -0600

    Add multithreaded repo sync

--- setup_build_folder.sh
+++ setup_build_folder.sh
@@ -5,13 +5,15 @@ echo "Enter branch:"
 read branch
 echo "Enter version (imx-<version>.xml)"
 read version
+echo "Enter desired bsp folder name:"
+read name
 
 cd /home/$USER
 
-mkdir imx-yocto-bsp
-cd imx-yocto-bsp
+mkdir $name
+cd $name
 /home/$USER/bin/repo init -u https://github.com/nxp-imx/imx-manifest -b $branch -m $version
-/home/$USER/bin/repo sync
+/home/$USER/bin/repo sync -j`nproc`
 
 echo "You may now set up a build folder. Please run the following command:"
 echo "cd ~/imx-yocto-bsp; DISTRO=<distro name> MACHINE=<machine name> source imx-setup-release.sh -b <build-dir>"
