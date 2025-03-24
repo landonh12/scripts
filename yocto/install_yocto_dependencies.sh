53d2c3f0bfb52ba1f042e65c05ce78f5f7f384d7
Landon <landon.haugh@nxp.com>
2025-03-24 13:01:20 -0500

    Update scripts

--- yocto/install_yocto_dependencies.sh
+++ yocto/install_yocto_dependencies.sh
@@ -6,7 +6,7 @@ echo "Installing dependent packages."
 
 sudo apt install gawk wget git diffstat unzip texinfo gcc build-essential \
 chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils \
-iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
+iputils-ping python3-git python3-jinja2 libsdl1.2-dev \
 python3-subunit mesa-common-dev zstd liblz4-tool file python-is-python3 locales -y
 
 sudo locale-gen en_US.UTF-8
--- yocto/setup_build_folder.sh
+++ yocto/setup_build_folder.sh
@@ -1,27 +1,21 @@
 echo "Setting up yocto"
 
-echo "Please go to github.com/nxp-imx/imx-manifest and choose the following:"
-echo "Enter branch:"
 echo "List of branches: "
-echo "1. imx-linux-nanbield LF6.6"
-echo "2. imx-linux-mickledore LF6.1.55/22"
-echo "3. imx-linux-langdale LF6.1.1"
-echo "4. imx-linux-kirkstone LF5.15.71/52/32"
-echo "5. imx-linux-honister LF5.15.5"
-echo "6. imx-linux-hardknott LF5.10.72/52/35"
+git ls-remote --heads --refs https://github.com/nxp-imx/imx-manifest imx-linux-*
+echo "Input branch:"
 read branch
+mkdir tmp
+git clone --depth 1 https://github.com/nxp-imx/imx-manifest -b $branch tmp/
+cd tmp/
 echo "Enter version (imx-<version>.xml)"
-echo "1. imx-6.6.3-1.0.0.xml"
-echo "2. imx-6.1.55-2.2.0.xml, imx-6.1.36-2.1.0.xml, imx-6.1.22-1.0.0.xml"
-echo "3. imx-6.1.1-1.0.0.xml"
-echo "4. imx-5.15.71-2.2.2.xml, imx-5.15.52-2.1.0.xml, imx-5.15.32-2.0.0.xml"
-echo "5. imx-5.15.5-1.0.0.xml"
-echo "6. imx-5.10.72-2.2.3.xml, imx-5.10.52-2.2.0.xml, imx-5.10.35-2.0.0.xml"
+ls imx-*.xml
 read version
 echo "Enter desired bsp folder name:"
 read name
 
-cd /home/$USER/work/yocto
+rm -rf tmp/
+
+cd /home/landon/work/yocto
 
 mkdir $name
 cd $name
@@ -29,6 +23,6 @@ repo init -u https://github.com/nxp-imx/imx-manifest -b $branch -m $version
 repo sync -j`nproc`
 
 echo "You may now set up a build folder. Please run the following command:"
-echo "cd ~/imx-yocto-bsp; DISTRO=<distro name> MACHINE=<machine name> source imx-setup-release.sh -b <build-dir>"
+echo "cd ~/imx-yocto-bsp; DISTRO=<distro> MACHINE=<machine name> source imx-setup-release.sh -b <build-dir>"
 echo "Here is a list of DISTRO names: fsl-imx-wayland, fsl-imx-xwayland, fsl,imx-fb"
 echo "For MACHINE names, please refer to the i.MX Yocto Project User's Guide for your BSP version."
